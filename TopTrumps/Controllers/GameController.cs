b17fddaf08fbfff3d74f0792407bf1fa79a5bfe1
marstc1 <christopher.marston@aviva.co.uk>
2014-08-19 17:44:20 +0100

    Now storing data in session

--- TopTrumps/Controllers/GameController.cs
+++ TopTrumps/Controllers/GameController.cs
@@ -42,22 +42,57 @@ namespace TopTrumps.Controllers
                 }
             }
 
-            if (players[0].Hand.FirstOrDefault().Strength < players[1].Hand.FirstOrDefault().Strength)
-            {
-                var card = players[0].Hand.FirstOrDefault();
-                players[0].Hand.Remove(card);
-                players[1].Hand.Add(card);
-                ViewBag.Message = string.Format("{0} wins", players[1].Name);
-            }
-            else if (players[0].Hand.FirstOrDefault().Strength > players[1].Hand.FirstOrDefault().Strength)
-            {
-                var card = players[1].Hand.FirstOrDefault();
-                players[1].Hand.Remove(card);
-                players[0].Hand.Add(card);
-                ViewBag.Message = string.Format("{0} wins", players[0].Name);
-            }
-            
+            this.HttpContext.Session["players"] = players;
+
             return View(players);
+        }
+
+        [HttpPost]
+        public ActionResult NewGame(string selectedOption)
+        {
+            var httpSessionStateBase = this.HttpContext.Session;
+            if (httpSessionStateBase != null)
+            {
+                var players = (List<Player>)httpSessionStateBase["players"];
+
+                var player1 = players[0];
+                var computer = players[1];
+
+                var playersCard = player1.Hand.FirstOrDefault();
+                var computersCard = computer.Hand.FirstOrDefault();
+
+                if (playersCard == null || computersCard == null)
+                {
+                    return this.RedirectToAction("Wins");
+                }
+
+                player1.Hand.Remove(playersCard);
+                computer.Hand.Remove(computersCard);
+
+                if (playersCard.Strength > computersCard.Strength)
+                {
+                    player1.Hand.Add(playersCard);
+                    player1.Hand.Add(computersCard);
+                    this.ViewBag.Message = string.Format("{0} wins", player1.Name);
+                }
+                else if (playersCard.Strength < computersCard.Strength)
+                {
+                    computer.Hand.Add(playersCard);
+                    computer.Hand.Add(computersCard);
+                    this.ViewBag.Message = string.Format("{0} wins", computer.Name);
+                }
+
+                httpSessionStateBase["players"] = players;
+
+                return this.View(players);
+            }
+
+            return this.RedirectToAction("NewGame");
+        }
+
+        public ActionResult Wins()
+        {
+            return this.View();
         }
     }
 }
