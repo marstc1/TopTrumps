27ea2f2560b0526f677cdd8396d9b1a693e3c374
marstc1 <christopher.marston@aviva.co.uk>
2014-09-04 16:52:17 +0100

    Added validation to NewGameViewModel

--- TopTrumps/Models/ViewModels/Game/NewGameViewModel.cs
+++ TopTrumps/Models/ViewModels/Game/NewGameViewModel.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace TopTrumps.Models.ViewModels.Game
+﻿namespace TopTrumps.Models.ViewModels.Game
 {
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// The New Game View Model
+    /// </summary>
     public class NewGameViewModel
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string PlayerName { get; set; }
 
         public int PackId { get; set; }
